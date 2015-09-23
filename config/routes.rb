df84572a9383107df3a26ab87ee5a04994f269a3
Brian Groat <Barnum7@users.noreply.github.com>
2015-09-23 17:48:58 -0400

    undid changes, need to fork to seperate branch

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
  resources :users, only: [:new, :create]
  
+end
  resources :sessions, only: [:new, :create, :destroy] 
  root to: 'movies#index'
   # The priority is based upon order of creation: first created -> highest priority.
