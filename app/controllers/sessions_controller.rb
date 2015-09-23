8b447325b2c40348ec3f346861617b1815625d8d
Brian Groat <Barnum7@users.noreply.github.com>
2015-09-23 13:32:27 -0400

    Added links to log in, sign up, or log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,6 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:email])
 
-  	if @user.save
-  		session[:user_id] = @user.id
-  		redirect_to movies_path
-  	else
-  		render :new
-  	end
-
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
   		redirect_to movies_path, notice: "Welcome back, #{user.firstname}!"
@@ -21,6 +14,11 @@ class SessionsController < ApplicationController
   		render :new
   	end
 
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to movies_path, notice: "Adios!"
   end
 
 end
